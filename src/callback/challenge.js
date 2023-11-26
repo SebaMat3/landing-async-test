26a6bf275df34d4cbf55e475ccfea3482b72612c
Sebastian Carballo <sebastiancarballoes@gmail.com>
2023-11-26 11:26:09 -0300

    Facing the challenge of calling an e-commerce API, which provides multiple resources. In this case we are capturing the following data: - Requesting for all the /products -> if the connection to the API succeds: - Request for a specific product -> if it succeeds as well: - Request its category - Finally showing this data, when the requests end. To achieve this, Im using XLMHttpRequest JS core object: a primitive, yet simple and effective way of handling requests to an URL, not just XML files, using plain JS Vanilla, no frameworks; not having to reload the whole website to bring the data: - After installing the dependencies with Node (npm i xmlhttprequest), importing them to a challenge.js script file and instantiating the object in it -> Ive proceeded to set the basic structure for a primary fetchData() function, which handles requests errors and responses receiving an API url and a callback function. - Then Im testing the use of this function with the Platzi API, giving a set of nested callback anonymous functions, with 3 consecutive requests. Welcome to callback hell

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -42,3 +42,24 @@ function fetchData (urlApi, callback) {
     // send the request.
     xhttp. send();
 }
+
+/* CALLBACK HELL: Calling fetchData with nested callbacks, to access multiple consecutive resources of our API: products> 1product> its category.
+(1st -> we're using `template literals for smooother URL concatenation`,
+2nd -> anonymous callback function of anonymous nested functions using the fetchData callback param structure (error,data)) */
+fetchData(`${API}/products`, function(error1, data1) {
+    //If the initial API calling fails, shut the whole process (return == break),
+    if (error1) {return console.log(error1);}
+    //otherwise recall fetchData, adding the needed text to the API URL for accessing a specific product.
+    fetchData(`${API}/products/${data1[0].id}`, function (error2, data2){
+        // repeat same logic,
+        if (error2) {return console.log(error2);}
+        // this time asking for the previously requested product category
+        fetchData(`${API}/products/${data2?.category?.id}`, function (error3, data3){
+            if (error3) {return console.log(error3);}
+            // If everything was found, print the requested objects.
+            console.log(data1[0]);
+            console.log(data2.title);
+            console.log(data3.name);
+        });
+    });
+});
